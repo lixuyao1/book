4bf7a53c1d5e2a304a2b34e6bc5a2251036378ae
lixuyao1 <10407945@qq.com>
2018-06-11 10:31:36 +0800

    add all

---
+++ app/Entity/PdContent.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Entity;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PdContent extends Model
+{
+     protected $table = 'pdt_content';
+    public $timestamps = false;
+}
---
+++ app/Entity/PdImages.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Entity;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PdImages extends Model
+{
+     protected $table = 'photo';
+    public $timestamps = false;
+}
